99380702b3d4c184750a2e1790749e59ad783e62
Jakub Pelczar <jpelczar@users.noreply.github.com>
2016-11-01 20:28:00 +0100

    Update make.sh

--- make.sh
+++ make.sh
@@ -2,5 +2,6 @@
 
 printf "\n-----------PDFLATEX-------------\n"
 pdflatex -synctex=1 -interaction=nonstopmode -output-directory=./build ./tex/$1.tex
+mkdir ./build
 mv ./build/$1.pdf ./pdf
 evince ./pdf/$1.pdf &
